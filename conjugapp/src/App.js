6b4eae5f78f4155c8794459f1616fb4e35e897bf
abarg12 <aidan.barg@gmail.com>
2022-05-22 22:03:55 -0400

    new logo, header bar, color scheme

--- conjugapp/src/App.js
+++ conjugapp/src/App.js
@@ -4,8 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="HeaderBar">
-        <p className="AppTitle">test test test</p>
+          <a href="./">
+            <img src="gator_logo2.png" className="SmallLogo" />
+          </a>
+          <p className="AppTitle">ConjuGator</p>
       </header>
+      <body>
+        <button type="button" className="MainButton">
+          Click Me!
+        </button>
+      </body>
     </div>
   );
 }
